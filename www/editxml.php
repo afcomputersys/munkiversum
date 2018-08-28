afd0476fa5042f564e453cac9f0e80c82946f16b
Michael Schuler <mschuler@a-f.ch>
2018-08-28 10:36:48 +0200

    typo

--- www/editxml.php
+++ www/editxml.php
@@ -1,20 +1,45 @@
 <?php
-#if(isset($_POST['submit'])) {
-#$xml=simplexml_load_file('VLC-3.0.3.plist');
-#$xml->xpath('/plist/dict/key[text()="category"]/following-sibling::*[1]')=$_POST['name'];
-#$handle=fopen("VLC-3.0.3.plist","wb");
-#fwrite($handle,$xml->asXML('3333.plist'));
-#fclose($handle);
-#}
 
-$xml=simplexml_load_file('VLC-3.0.3.plist');
-echo $xml->xpath('/plist/dict/key[text()="category"]');
-$welcome=$xml->xpath('/plist/dict/key[text()="category"]/following-sibling::*[1]');
+    if( ! $xml = simplexml_load_file('VLC-3.0.3.plist') )
+    {
+        echo 'unable to load XML file';
+    }
+    else
+    {
 
+      if(isset($_POST['submit'])) {
+      $cat = $xml->xpath('/plist/dict/key[text()="catalogs"]/following-sibling::*[1]')[0];
+      $cat->string[0]=$_POST['name'];
+      $xml->asXML('VLC-3.0.3.plist');
+      }
+
+      echo 'XML file loaded successfully<br>';
+      echo '-----------------------------------------------------------------<br>';
+      print_r( $xml->xpath('/plist/dict/key[text()="catalogs"]/following-sibling::*[1]') );
+      $cat = $xml->xpath('/plist/dict/key[text()="catalogs"]/following-sibling::*[1]');
+      echo '<br>'.$cat[0]->string[0]; #testing
+      echo '<br>'.$cat[0]->string[1]; # production
+      echo '<br><br>';
+      echo '<br><br>';
+
+
+      echo '-----------------------------------------------------------------<br>';
+      $i = 0;
+      $cat = $xml->xpath('/plist/dict/key[text()="catalogs"]/following-sibling::*[1]')[0];
+      foreach( $cat as $element)
+          {
+      echo 'CatalogsString '.$i.': '.($cat->string[$i]);
+      echo '<br>-----------------------------------------------------------------<br>';
+      $i++;
+      }
+      # $cat->string[0]='newElement'; # editiert ersten String
+
+      $xml->asXML('VLC-3.0.3.plist');
+    }
 ?>
 
 <form method="post">
-    <textarea name="name"><?php echo $welcome ?></textarea>
+    <textarea name="name"><?php echo $cat->string[0] ?></textarea>
     <br>
     <input type="submit" name="submit" value="submit">
 </form>
