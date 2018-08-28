79b1855d6a44368f4ff335f995ed5ed02e3c8b57
Michael Schuler <mschuler@a-f.ch>
2018-08-28 14:34:21 +0200

    mit Folderlisting

--- www/editxml.php
+++ www/editxml.php
@@ -1,59 +1,44 @@
 <?php
-# 3 Catalogs möglich: new, testing, production
+# 3 Catalogs möglich: new, testing, production (könnte auch allgemein programmiert wreden)
 
-$PlistFile = 'VLC-3.0.3.plist';
-
-    if( ! $xml = simplexml_load_file($PlistFile) )
-    {
-        echo 'unable to load XML file';
-    }
-    else
-    {
-      if(isset($_POST['submit'])) {
-      # Entfernt den Inhalt des Catalogs-Array
-      unset($xml->xpath('/plist/dict/key[text()="catalogs"]/following-sibling::*[1]')[0]->string);
-      $cat = $xml->xpath('/plist/dict/key[text()="catalogs"]/following-sibling::*[1]')[0];
+$directory = 'pkgsinfo';
+$scanned_directory = array_diff(scandir($directory), array('..', '.'));
+foreach ($scanned_directory as $value) {
+    $PlistFile = $value;
+    $PlistFileShorten = preg_replace("/[^A-Za-z0-9]/", "", $PlistFile);
+    if(!$xml=simplexml_load_file($directory.'/'.$PlistFile)) {
+      echo 'unable to load XML file: '.$PlistFile; }
+    else {
+      $displayname = $xml->xpath('/plist/dict/key[text()="display_name"]/following-sibling::*[1]')[0];
+      $version = $xml->xpath('/plist/dict/key[text()="version"]/following-sibling::*[1]')[0];
+      if(isset($_POST['submit'.$PlistFileShorten])) {
+        # Entfernt den Inhalt des Catalogs-Array
+        unset($xml->xpath('/plist/dict/key[text()="catalogs"]/following-sibling::*[1]')[0]->string);
+        $cat = $xml->xpath('/plist/dict/key[text()="catalogs"]/following-sibling::*[1]')[0];
+        # macht Strings der ausgewählten Checkboxen am richtigen Ort mit der korrekten Nummerierung
         $a = 0;
         if(isset($_POST['new'])) {
-          $cat->string[$a]=$_POST['new']; $a++;}
+          $cat->string[$a]=$_POST['new']; $a++; }
         if(isset($_POST['testing'])) {
-          $cat->string[$a]=$_POST['testing']; $a++;}
+          $cat->string[$a]=$_POST['testing']; $a++; }
         if(isset($_POST['production'])) {
-          $cat->string[$a]=$_POST['production']; $a++;}
-        $xml->asXML($PlistFile); #speichert das File zurück
-      }
-
-      echo 'XML file loaded successfully<br>';
-      echo '-----------------------------------------------------------------<br>';
-      $cat = $xml->xpath('/plist/dict/key[text()="catalogs"]/following-sibling::*[1]')[0];
-      print_r( $cat );
-      echo '<br>-----------------------------------------------------------------<br>';
-      echo '<br><br>';
-
-
-      $i = 0;
-      $cat = $xml->xpath('/plist/dict/key[text()="catalogs"]/following-sibling::*[1]')[0];
-      # echo '<br>'.$cat[0]->string[0]; # testing
-      # echo '<br>'.$cat[0]->string[1]; # production
-      echo '-----------------------------------------------------------------<br>';
-      foreach( $cat as $element)
-      {
-      echo 'CatalogsString '.$i.': '.($cat->string[$i]);
-      echo '<br>-----------------------------------------------------------------<br>';
-      $i++;
-      }
-      # $cat->string[0]='newElement'; # editiert ersten String
-      $displayname = $xml->xpath('/plist/dict/key[text()="display_name"]/following-sibling::*[1]')[0];
+          $cat->string[$a]=$_POST['production']; $a++; }
+        #speichert das File zurück
+        $xml->asXML($directory.'/'.$PlistFile); }
+      else {
+        $cat = $xml->xpath('/plist/dict/key[text()="catalogs"]/following-sibling::*[1]')[0]; }
     }
 ?>
 
-
-
 <form method="post">
-    <?php echo 'DisplayName: '.$displayname.'<br>';?>
-    <input type="checkbox" name="new" value="new" <?php if (stripos(json_encode($cat),'new') !== false) {echo "checked";}?>> new<br>
-    <input type="checkbox" name="testing" value="testing" <?php if (stripos(json_encode($cat),'testing') !== false) {echo "checked";}?>> testing<br>
-    <input type="checkbox" name="production" value="production" <?php if (stripos(json_encode($cat),'production') !== false) {echo "checked";}?>> production<br>
-    <br>
-    <input type="submit" name="submit" value="submit">
+    <?php echo $displayname.' '.$version.'<br>';?>
+    <input type="checkbox" name="new" value="new" <?php if (stripos(json_encode($cat),'new') !== false) {echo "checked";}?>> new ---
+    <input type="checkbox" name="testing" value="testing" <?php if (stripos(json_encode($cat),'testing') !== false) {echo "checked";}?>> testing ---
+    <input type="checkbox" name="production" value="production" <?php if (stripos(json_encode($cat),'production') !== false) {echo "checked";}?>> production ---
+    <input type="submit" name="<?php echo 'submit'.$PlistFileShorten; ?>" value="submit">
+    <hr>
 </form>
+
+<?php
+}
+?>
